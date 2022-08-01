5606216cc8c31ddd526aabd840847a5e51faed9f
vitordwb <vitordwb@gmail.com>
2022-08-01 14:41:43 -0300

    first commit

---
+++ dist/pwa/js/2d0e8be2.b9be60c4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["2d0e8be2"],{"8b24":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div")},i=[],r=(n("a481"),{name:"PageIndex",mounted:function(){this.$router.replace({name:"soundBoard",params:this.initialCategory})},computed:{initialCategory:function(){return this.$store.getters["menu/initialCategory"][0]}}}),o=r,u=n("2877"),s=Object(u["a"])(o,a,i,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=2d0e8be2.b9be60c4.js.map
---
+++ dist/pwa/precache-manifest.a5a0da1bd2603616e6cdd62e42ba65ab.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff482575575c4d7c30d",
+    "url": "css/app.2a3d62ca.css"
+  },
+  {
+    "revision": "b1e81c01bc6888d2ef12f781c98fe3ec",
+    "url": "css/app.2a3d62ca.css.gz"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "1a95531b09ffc9b76104e0003b456a0e",
+    "url": "fonts/materialdesignicons-webfont.1a95531b.woff"
+  },
+  {
+    "revision": "84c6fe1e4353c916d8f1452e82bdef8d",
+    "url": "fonts/materialdesignicons-webfont.84c6fe1e.woff2"
+  },
+  {
+    "revision": "e839385a297a56378ff9f581403af67d",
+    "url": "index.html"
+  },
+  {
+    "revision": "aa6160b0b2e91dd6163b",
+    "url": "js/2d0db882.82e3230b.js"
+  },
+  {
+    "revision": "23515ebbf8801f1fa7b7",
+    "url": "js/2d0e8be2.b9be60c4.js"
+  },
+  {
+    "revision": "e633cdf59e5bc348bf0d",
+    "url": "js/2d20fc9a.65fa87c3.js"
+  },
+  {
+    "revision": "59a6db2f953e489340cd",
+    "url": "js/4b47640d.e9613c82.js"
+  },
+  {
+    "revision": "7c9f6f36e8cc12c85171bb30074d4051",
+    "url": "js/4b47640d.e9613c82.js.gz"
+  },
+  {
+    "revision": "aff482575575c4d7c30d",
+    "url": "js/app.531655cc.js"
+  },
+  {
+    "revision": "faaa3067696a3cd1335dc323c8cb6a3b",
+    "url": "js/app.531655cc.js.gz"
+  },
+  {
+    "revision": "2f3b4983d8d4c61b74d8",
+    "url": "js/vendor.d5b3590c.js"
+  },
+  {
+    "revision": "ba500ee2068876b8dfe2cfbf7ed1792a",
+    "url": "js/vendor.d5b3590c.js.gz"
+  },
+  {
+    "revision": "c9ba9a090b0ea0045ef6a41283182769",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "e7fef6f5ed334ccebde7236ec3e9aa34",
+    "url": "statics/audios/effects/grilo.mp3"
+  },
+  {
+    "revision": "0a8de67321bb7c9032c9eec2de47a4f7",
+    "url": "statics/audios/faustao/ce-vai-morre-faustao.mp3"
+  },
+  {
+    "revision": "faa89d0a845983e80f998a5fc4ffad84",
+    "url": "statics/audios/faustao/churrasqueira-eletrica-pegando-fogo-ao-vivo-no-faustao.mp3"
+  },
+  {
+    "revision": "6902bfe1efdfe5ab20fcef08e8746cfe",
+    "url": "statics/audios/faustao/domingao-do-faustao-arquivo-confidencial-convert-video-online.mp3"
+  },
+  {
+    "revision": "f1a0da3779b28cef62f66c5fbc59d853",
+    "url": "statics/audios/faustao/errou.mp3"
+  },
+  {
+    "revision": "f9a92ffdc1f84726652571b286397f68",
+    "url": "statics/audios/faustao/frango_tJTQwzg.mp3"
+  },
+  {
+    "revision": "1faa818f92a17bebcb69f0895543a41c",
+    "url": "statics/audios/faustao/horadealegria.mp3"
+  },
+  {
+    "revision": "c730c092c6dafdab96fc2b176bc6a2d4",
+    "url": "statics/audios/faustao/mas-oloko-bicho.mp3"
+  },
+  {
+    "revision": "17376792b6a29820ba3171eb7cf67938",
+    "url": "statics/audios/faustao/olokinho-meu-faustaozinho.mp3"
+  },
+  {
+    "revision": "87f629cd01a90047b03b56ac6d384836",
+    "url": "statics/audios/faustao/porra-meu-ala.mp3"
+  },
+  {
+    "revision": "3d006571f5908ead606f3d59bcb39bdf",
+    "url": "statics/audios/faustao/retardado.mp3"
+  },
+  {
+    "revision": "3f3f8f86527d1bfb0acb96341ba6bae5",
+    "url": "statics/audios/games/blow copy.wav"
+  },
+  {
+    "revision": "c20d4d49c3f3ac6c02f81de34632f99d",
+    "url": "statics/audios/games/blow.wav"
+  },
+  {
+    "revision": "8e477d89479ad5f5ab0bfcf09679bac5",
+    "url": "statics/audios/games/bombdef.wav"
+  },
+  {
+    "revision": "65cb7104f625165ed7790de5b72554cc",
+    "url": "statics/audios/games/bombpl copy.wav"
+  },
+  {
+    "revision": "31c86dad80747433bd2ceb8cbb19953a",
+    "url": "statics/audios/games/bombpl.wav"
+  },
+  {
+    "revision": "98467a157762af0e41f49b72c7055ef5",
+    "url": "statics/audios/games/com_go.wav"
+  },
+  {
+    "revision": "8d4084d71615df401c546cbaab24ebb0",
+    "url": "statics/audios/games/ct_backup.wav"
+  },
+  {
+    "revision": "46d20e33f2328ae9d0648f43fd61867f",
+    "url": "statics/audios/games/ct_fireinhole.wav"
+  },
+  {
+    "revision": "f60fc56ddc3ee3709c38dbf0e532af47",
+    "url": "statics/audios/games/mario-1-up.mp3"
+  },
+  {
+    "revision": "d4e94afcbb3686f50232ad31979b5a43",
+    "url": "statics/audios/games/wololo.mp3"
+  },
+  {
+    "revision": "6863d10a69a6b3c41b33e345fab6ab42",
+    "url": "statics/audios/impact/enemydown.wav"
+  },
+  {
+    "revision": "d713df5bb0320a9501a9a56454add8e8",
+    "url": "statics/audios/impact/foda.mp3"
+  },
+  {
+    "revision": "1f0e0d5ed39f547e6733fdb0cbe76b29",
+    "url": "statics/audios/impact/hos2.wav"
+  },
+  {
+    "revision": "f094d064e7306b45bfe02d97e46de093",
+    "url": "statics/audios/impact/jaaviseiquevaidarmerda.mp3"
+  },
+  {
+    "revision": "87a31c4e9c60626dd0396c1e367f11ed",
+    "url": "statics/audios/impact/negative.wav"
+  },
+  {
+    "revision": "36981622ca2875eebab0263b96a45ea1",
+    "url": "statics/audios/impact/nossa-q-bosta.mp3"
+  },
+  {
+    "revision": "c85a5cc538daaf1dc251663a5c05b9dc",
+    "url": "statics/audios/impact/roger.wav"
+  },
+  {
+    "revision": "051432de00ec8a64fc77b971de5a1508",
+    "url": "statics/audios/impact/vai-dar-merda-vaaaai.mp3"
+  },
+  {
+    "revision": "eddc31bb5ce37f9cc129a1234950595c",
+    "url": "statics/audios/impact/vinheta-galvao-do-tetra.mp3"
+  },
+  {
+    "revision": "6fd90f5f908589e63f7f280262542c4c",
+    "url": "statics/audios/meme/hitler-nein_1.mp3"
+  },
+  {
+    "revision": "4b7a3e8854e1f1b512f44756ee96e031",
+    "url": "statics/audios/meme/oooo-o-gas-oia-o-gas.mp3"
+  },
+  {
+    "revision": "560e7a1c9f396d9f7b665caa76fa491f",
+    "url": "statics/audios/meme/pepino.mp3"
+  },
+  {
+    "revision": "5e103e3da583b0a8040fdeef96a4df82",
+    "url": "statics/audios/music/wow_8.mp3"
+  },
+  {
+    "revision": "7bc7150a173a9c6ae44ce3232e7d3986",
+    "url": "statics/audios/nsfw/chico-bioca-quem-e-que-transa-nessa-porra.mp3"
+  },
+  {
+    "revision": "9c476c44da4334932c8debade6e50bd8",
+    "url": "statics/audios/nsfw/cu-dando-bote.mp3"
+  },
+  {
+    "revision": "4fdfc5be994550be550a809d534eed60",
+    "url": "statics/audios/nsfw/eita-porra-do-caralho_Y4pbH2I.mp3"
+  },
+  {
+    "revision": "5b1b82ac1cdbbbb254722bc76aedb9a3",
+    "url": "statics/audios/nsfw/fecha-o-c-pra-falar-comigo-away.mp3"
+  },
+  {
+    "revision": "437f758317a3549c311800c15a082369",
+    "url": "statics/audios/nsfw/pai-de-familia-delicia-cara.mp3"
+  },
+  {
+    "revision": "4e34df34b61fec37293caa5990671be5",
+    "url": "statics/audios/nsfw/que-peido-gostoso-agora-goza-mp3cut.mp3"
+  },
+  {
+    "revision": "6c5af1b98a1dbad1e548f399042f2bf4",
+    "url": "statics/audios/nsfw/tudo-puta-original.mp3"
+  },
+  {
+    "revision": "62a134ea8c7d9b796956feb2dd9a0e41",
+    "url": "statics/audios/nsfw/vale_tudo_gil.mp3"
+  },
+  {
+    "revision": "cc1ab3d82e5343972b3206a26082c5d4",
+    "url": "statics/audios/tv/Aham-Claudia-Senta-La.mp3"
+  },
+  {
+    "revision": "44c753a7203a583101b763f3e569f03f",
+    "url": "statics/audios/tv/ae-kasinao_1.mp3"
+  },
+  {
+    "revision": "282a3f12aab1849f7f65bd2ddfcf4398",
+    "url": "statics/audios/tv/e-a-ambulancia-besta000.mp3"
+  },
+  {
+    "revision": "ef5c855c03acd9641df654c0f2f27435",
+    "url": "statics/audios/tv/e-morreu-didi.mp3"
+  },
+  {
+    "revision": "afc913beaf42664a8fd317e24140d6c1",
+    "url": "statics/audios/tv/eu-sou-rica_2.mp3"
+  },
+  {
+    "revision": "b0f84596a917cb9137e6ab71bae177df",
+    "url": "statics/audios/tv/ha-ha-risada-do-nelson.mp3"
+  },
+  {
+    "revision": "71a6e8304972e1f9ee101ae52c280e20",
+    "url": "statics/audios/tv/mentira_3.mp3"
+  },
+  {
+    "revision": "77fb9f09b862b0258d6d2f15f464e1ad",
+    "url": "statics/audios/tv/no_ceu_tem_pao.mp3"
+  },
+  {
+    "revision": "3b3a6c92a99aa3ff25913b39a19dfaea",
+    "url": "statics/audios/tv/quepena.mp3"
+  },
+  {
+    "revision": "ccca2a5f37165f19272c093deaf06241",
+    "url": "statics/audios/youtube/agnaldo_te_mostro_menino_ou_menina_e01-mp3cut.mp3"
+  },
+  {
+    "revision": "6010c00b2e87e4eded4531db433fce48",
+    "url": "statics/audios/youtube/ai-pai-para-hihi.mp3"
+  },
+  {
+    "revision": "754aeaf51b224bc7fa423159cd74ce53",
+    "url": "statics/audios/youtube/ajuda-o-maluco-que-ta-doente.mp3"
+  },
+  {
+    "revision": "1c9da1335358dd28de56aab1413059f8",
+    "url": "statics/audios/youtube/bota-no-alto-porra.mp3"
+  },
+  {
+    "revision": "30e33a6d79b459f8d132c9d3f6c1de97",
+    "url": "statics/audios/youtube/ce-e-o-bichao-memo-hein-doido-red.mp3"
+  },
+  {
+    "revision": "33136e3a69558c0db8820f07af0b9d20",
+    "url": "statics/audios/youtube/errou-rude.mp3"
+  },
+  {
+    "revision": "b1283d0e51609d55a2ea50791b4366fe",
+    "url": "statics/audios/youtube/hd-epic-sax-gandalf-10-sec.mp3"
+  },
+  {
+    "revision": "a9febc8a5557017b565575fbabb86dca",
+    "url": "statics/audios/youtube/ola-marilenes.mp3"
+  },
+  {
+    "revision": "c549429128650374d0b4d1f3bb0927eb",
+    "url": "statics/audios/youtube/processinho.mp3"
+  },
+  {
+    "revision": "329382c947a95854e9e90d68d252df19",
+    "url": "statics/audios/youtube/serjao-berranteiro-matador-de-onca.mp3"
+  },
+  {
+    "revision": "0581eff1694b8806832545018d41de2c",
+    "url": "statics/audios/youtube/vem-monstro.mp3"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3f5de7654b25e5f1640662111d28b490",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "15bc9eca3421a7d5a0599a5fb39fe7a8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f309ab32f7ba3a2d3f50d05ba3d2aaa2",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c8fdfe20437ccd115ee07cb684685ded",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  }
+]);
